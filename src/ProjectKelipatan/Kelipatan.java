67282242dc49dd379dbf8a93dd5c8df95ac37fbd
LENOVO <LENOVO@DESKTOP-VL4046V>
2023-10-27 22:02:58 +0700

    Project PBO PROGRAM KELIPATAN

---
+++ src/ProjectKelipatan/Kelipatan.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ProjectKelipatan;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Kelipatan {
+    public static void main(String[] args) {
+        double awal = 3.5;
+        double akhir = 35.0;
+
+        System.out.println("Bilangan kelipatan desimal 3.5 antara " + awal + " dan " + akhir + " adalah:");
+
+        for (double i = awal; i <= akhir; i += 3.5) {
+            System.out.println(i);
+        }
+    }
+}
