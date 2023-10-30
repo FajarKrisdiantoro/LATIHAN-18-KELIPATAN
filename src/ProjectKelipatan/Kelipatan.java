6f975d5e9e277e19ebcc3a9fbf63ad9c5bbf17d8
LENOVO <LENOVO@DESKTOP-VL4046V>
2023-10-30 21:51:46 +0700

    SECOND commit PROGRAM KELIPATAN Menambahkan identitas

--- src/ProjectKelipatan/Kelipatan.java
+++ src/ProjectKelipatan/Kelipatan.java
@@ -2,6 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*
+NAMA : FAJAR KRISDIANTORO
+NIM  : 22205004
+PRODI: TI REG SORE
+*/
+
 package ProjectKelipatan;
 
 /**
